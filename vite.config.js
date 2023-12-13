9c33a3faae44c04517aa3069910011a0a285b6c6
xinpei930729 <yugioh930729@gmail.com>
2023-12-13 17:55:31 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+const outDir = resolve(__dirname, 'docs')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/homework1/',
+  build: {
+    outDir
+  }
 })
